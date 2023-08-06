6133215b16c84613127f0f6f26259efe68090e58
wojciech <wwalczakpraca@wp.pl>
2023-08-06 13:08:55 +0200

    Add class DateInfo

---
+++ src/main/java/pl/isa/models/DateInfo.java
@@ -0,0 +1,13 @@
+package pl.isa.models;
+
+public class DateInfo {
+    private int year;
+    private int month;
+    private int day;
+
+    public DateInfo(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+
+}
