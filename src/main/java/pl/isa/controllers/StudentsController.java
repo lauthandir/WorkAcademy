6bf8dcc6062e614dfc75f705f37cc2ca31f6fb83
wojciech <wwalczakpraca@wp.pl>
2023-08-06 18:19:31 +0200

    add new dependencies to pom, and create StudentController code

--- src/main/java/pl/isa/controllers/StudentsController.java
+++ src/main/java/pl/isa/controllers/StudentsController.java
@@ -1,9 +1,26 @@
 package pl.isa.controllers;
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import pl.isa.models.StudentModel;
+import pl.isa.studentsdatabase.StudentsData;
+import java.util.List;
+
+
+@Path("/students")
 public class StudentsController {
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<StudentModel> getStudents() {
+    return StudentsData.readStudentData();
+    }
 
-
-
-
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response saveStudent(StudentModel student) {
+    StudentsData.saveStudentData(student);
+    return Response.status(Response.Status.CREATED).build();
+    }
 }
