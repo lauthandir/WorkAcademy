fdd082127b08fc3ca1867f259241f15b18677a19
wojciech <wwalczakpraca@wp.pl>
2023-08-06 13:36:37 +0200

    Set class LocalDateDeserializer

--- src/main/java/pl/isa/LocalDateDeserializer.java
+++ src/main/java/pl/isa/LocalDateDeserializer.java
@@ -1,6 +1,17 @@
 package pl.isa;
 
-public class LocalDateDeserializer {
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
+public class LocalDateDeserializer implements JsonDeserializer<LocalDate> {
+    @Override
+    public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)  {
+        return LocalDate.parse(json.getAsString(),
+                DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
 }
