1fa4ca34f10d65135a4d5e96c4184e0daad9c97f
wojciech <wwalczakpraca@wp.pl>
2023-08-08 15:33:48 +0200

    correction student

--- src/main/java/pl/isa/models/StudentModel.java
+++ src/main/java/pl/isa/models/StudentModel.java
@@ -1,24 +1,38 @@
 package pl.isa.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
 import java.time.LocalDate;
 
 @Entity
 public class StudentModel {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
+    @JsonProperty
     private String name;
+    @JsonProperty
     private String surname;
+    @JsonProperty
     private LocalDate dateBirth;
+    @JsonProperty
     private String course;
+
 
     public Long getId() {
         return Id;
     }
 
+
     public void setId(Long id) {
         this.Id = id;
     }
+
 
     public String getName() {
         return name;
