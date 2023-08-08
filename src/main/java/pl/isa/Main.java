1e97131e1fc5032ae33813ef50259f0a45582c45
wojciech <wwalczakpraca@wp.pl>
2023-08-08 15:13:39 +0200

    add jakcon config and correction

--- src/main/java/pl/isa/Main.java
+++ src/main/java/pl/isa/Main.java
@@ -1,49 +1,49 @@
-package pl.isa;
-
-import pl.isa.models.StudentModel;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        StudentModel student = new StudentModel();
-        System.out.println("Please fill in the required data below:");
-
-        System.out.print("1.Name: ");
-        String name = scanner.nextLine();
-
-        System.out.print("2.Surname: ");
-        String surname = scanner.nextLine();
-
-        LocalDate dateBirth = null;
-        while (true) {
-            try {
-                System.out.print("3.Date of Birth (yyyy-MM-dd): ");
-                String dateBirthString = scanner.nextLine();
-                dateBirth = LocalDate.parse(dateBirthString);
-                break;
-            } catch (DateTimeParseException e) {
-                System.out.println("Invalid format. Use the format yyyy-MM-dd.");
-            }
-        }
-
-        System.out.print("4.Course: ");
-        String course = scanner.nextLine();
-
-        student.setName(name);
-        student.setSurname(surname);
-        student.setCourse(course);
-        student.setDateBirth(dateBirth);
-
-        System.out.println();
-        System.out.println("Student Information:");
-        System.out.println("Name: " + student.getName());
-        System.out.println("Surname: " + student.getSurname());
-        System.out.println("Date of Birth: " + student.getDateBirth());
-        System.out.println("Course: " + student.getCourse());
-
-
-    }
-}
+//package pl.isa;
+//
+//import pl.isa.models.StudentModel;
+//import java.time.LocalDate;
+//import java.time.format.DateTimeParseException;
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        StudentModel student = new StudentModel();
+//        System.out.println("Please fill in the required data below:");
+//
+//        System.out.print("1.Name: ");
+//        String name = scanner.nextLine();
+//
+//        System.out.print("2.Surname: ");
+//        String surname = scanner.nextLine();
+//
+//        LocalDate dateBirth = null;
+//        while (true) {
+//            try {
+//                System.out.print("3.Date of Birth (yyyy-MM-dd): ");
+//                String dateBirthString = scanner.nextLine();
+//                dateBirth = LocalDate.parse(dateBirthString);
+//                break;
+//            } catch (DateTimeParseException e) {
+//                System.out.println("Invalid format. Use the format yyyy-MM-dd.");
+//            }
+//        }
+//
+//        System.out.print("4.Course: ");
+//        String course = scanner.nextLine();
+//
+//        student.setName(name);
+//        student.setSurname(surname);
+//        student.setCourse(course);
+//        student.setDateBirth(dateBirth);
+//
+//        System.out.println();
+//        System.out.println("Student Information:");
+//        System.out.println("Name: " + student.getName());
+//        System.out.println("Surname: " + student.getSurname());
+//        System.out.println("Date of Birth: " + student.getDateBirth());
+//        System.out.println("Course: " + student.getCourse());
+//
+//
+//    }
+//}
--- src/main/java/pl/isa/models/StudentList.java
+++ src/main/java/pl/isa/models/StudentList.java
@@ -1,21 +1,21 @@
-package pl.isa.models;
-
-import jakarta.persistence.Entity;
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-public class StudentList {
-    public List<StudentModel> students;
-
-    public StudentList() {
-        students = new ArrayList<>();
-    }
-    public List<StudentModel> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<StudentModel> students) {
-        this.students = students;
-    }
-}
+//package pl.isa.models;
+//
+//import jakarta.persistence.Entity;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//public class StudentList {
+//    public List<StudentModel> students;
+//
+//    public StudentList() {
+//        students = new ArrayList<>();
+//    }
+//    public List<StudentModel> getStudents() {
+//        return students;
+//    }
+//
+//    public void setStudents(List<StudentModel> students) {
+//        this.students = students;
+//    }
+//}
